70fd7340df2c03dd9075f29afc404677fc530135
TensaCoder <herschel.menezes00@gmail.com>
2022-08-18 21:04:12 +0530

    Footer and Header CSS Updation

--- Client/src/Components/Footer.js
+++ Client/src/Components/Footer.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import '../CSS/Footer.css'
-import {AiFillLinkedin, AiFillGithub, AiFillMail} from 'react-icons/ai';
+import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai';
 
 
 function Footer() {
   return (
-    <div className='footer'>
-      <div className="left">
-        <h5> About this Application</h5>
-        <p>We can add any items or list of items that needs to be completed with the help of the To-Do Application.</p>
-        <p>Once the work regarding the item is completed, we can delete the item from the list.</p>
-        <p>To add a new Item - Click on the + button.</p>
-        <p>To delete an item - Click the button next to the corresponding Item.</p>
+    <>
+      <div className='footer'>
+        <div className="left">
+          <h5> About this Application</h5>
+          <p>We can add any items or list of items that needs to be completed with the help of the To-Do Application.</p>
+          <p>Once the work regarding the item is completed, we can delete the item from the list.</p>
+          <p>To add a new Item - Click on the + button.</p>
+          <p>To delete an item - Click the button next to the corresponding Item.</p>
+        </div>
+        <div className="right">
+          <h5>Contact Me</h5>
+          <a href="https://in.linkedin.com/in/herschel-menezes-93abb6227"> <AiFillLinkedin size='2em' /> </a>
+          <a href="https://github.com/TensaCoder"> <AiFillGithub size='2em' /> </a>
+          <a href="mailto:herschel.menezes00@gmail.com"> <AiFillMail size='2em' /> </a>
+        </div>
+        <h6 id='footer-end'>@2022 Herschel Menezes</h6>
       </div>
-      <div className="right">
-        <h5>Contact</h5>
-        <a href="https://in.linkedin.com/in/herschel-menezes-93abb6227"> <AiFillLinkedin size='2em'/> </a>
-        <a href="https://github.com/TensaCoder"> <AiFillGithub size='2em'/> </a>
-        <a href="mailto:herschel.menezes00@gmail.com"> <AiFillMail size='2em'/> </a>
-      </div>
-    </div>
+    </>
   )
 }
 
--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -5,17 +5,17 @@ const Header = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">TO-DO Application</a>
+        <a class="navbar-brand" href="">TO-DO Application</a>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
+            {/* <li class="nav-item">
               <a class="nav-link" href="#">About</a>
-            </li>
+            </li> */}
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+              <a class="nav-link" to=''>Contact</a>
             </li>
           </ul>
         </div>
